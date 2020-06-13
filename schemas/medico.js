7aacaa9f0751a4735c603c9a0d9ceab4fa8abc5d
Karina Fernandez <karina.fernandez@pedidosya.com>
2020-06-13 19:43:14 -0300

    Validaciones

--- schemas/medico.js
+++ schemas/medico.js
@@ -9,18 +9,18 @@ const medicoSchema = Persona.discriminator('Medico', new Schema({
         type: String,
         required: [true, 'requerido'],
     },
-    especialidades: [{
-        type: String,
+    especialidades: {
+        type: [String],
         required: [true, 'requerido'],
-    }],
+    },
     puntuacion: {
         type: Number,
         match: [/^([1-9]|10)$/, 'Puntación invalida. Se espera valor del 1 al 10']
     },
-    comentarios: [{
-        type: Schema.Types.ObjectId,
+    comentarios: {
+        type: [Schema.Types.ObjectId],
         ref: 'Comentario'
-    }],
+    },
     centroMedico: {
         type: String,
         required: [true, 'requerido']
